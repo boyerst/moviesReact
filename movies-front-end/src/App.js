44763a59332bf3940b1caa042a912a178c5fb38b
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-27 18:43:46 -0500

    it renders a MovieContainer

--- movies-front-end/src/App.js
+++ movies-front-end/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MovieContainer from './MovieContainer'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <MovieContainer />
     </div>
   );
 }
---
+++ movies-front-end/src/MovieContainer/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class DogContainer extends Component {
+
+  render() {
+    return(
+      <h2>Movie Container</h2>
+    )
+  }
+
+}
