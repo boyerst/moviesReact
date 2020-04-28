c31c8829b0644eb4622919f246da5e6d823f3d8d
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-27 19:37:16 -0500

    implemented CORS, it fetches data from API when MovieContainer mounts

--- movies-front-end/src/App.js
+++ movies-front-end/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MovieContainer from './MovieContainer'
 
 function App() {
-  console.log(process.env)
+  
   return (
     <div className="App">
       <MovieContainer />
--- movies-front-end/src/MovieContainer/index.js
+++ movies-front-end/src/MovieContainer/index.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
 
 
-export default class DogContainer extends Component {
+export default class MovieContainer extends Component {
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+      movies: []
+    }
+  }
+
+
+  componentDidMount() {
+    this.getMovies()
+  }
+
+  getMovies = async () => {
+    try {
+      const url = process.env.REACT_APP_API_URL + "/api/v1/movies/"
+      console.log("about to fetch data from:");
+      console.log(url);
+      const moviesResponse = await fetch(url)
+      console.log("here is the Response from the fetch call:");
+      console.log(moviesResponse);
+      const moviesJson = await moviesResponse.json()
+      console.log("here is the data we got in getDogs in MovieContainer:");
+      console.log(moviesJson);
+    } catch(err) {
+      console.log("Error getting movie data", err)
+    }
+  }
 
   render() {
     return(
